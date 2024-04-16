d2757cc8711b90080906491e5388ce2a96ca1788
Adam Bedient <adambedient@gmail.com>
2024-04-16 08:47:29 -0400

    Add snake game to error

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -6,6 +6,7 @@ export default function LandingPage({ filterSnakes }) {
     return (
         <div className="landing">
             <h1>No Snake Zones</h1>
+            <h3>An emotional coping tool designed by someone with no credentials</h3>
             <LevelForm filterSnakes={filterSnakes} />
         </div>
     )
