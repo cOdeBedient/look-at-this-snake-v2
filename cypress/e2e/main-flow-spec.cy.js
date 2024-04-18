1ec39ba1a799d48bab786b3ffabc2fa27f20061f
Adam Bedient <adambedient@gmail.com>
2024-04-18 12:43:00 -0400

    Validate landing page elements

--- cypress/e2e/main-flow-spec.cy.js
+++ cypress/e2e/main-flow-spec.cy.js
@@ -1,5 +1,6 @@
 describe('LATSTLATP', () => {
   it('navigates to game page and validates all elements', () => {
+
     cy.fixture('snakes.json').then(snakes => {
       cy.intercept('GET', 'http://localhost:3000/api/v1/snakes/', {
         status: 201,
@@ -15,5 +16,15 @@ describe('LATSTLATP', () => {
     })
 
     cy.visit('http://localhost:3001/')
+      .get('.landing').contains('h1', "Look at this Snake then Look at this Puppy")
+      .get('.landing').contains('h2', "A phobic deprogramming tool designed by someone with no credentials")
+      .get('.level-form').contains('label', 'Level 1 |')
+      .get('.level-form').contains('label', 'Level 2 |')
+      .get('.level-form').contains('label', 'Level 3 |')
+      .get('.level-form').contains('label', 'Level 4 |')
+      .get('a').contains("p", "breathe. . . then click this button to begin")
+
+
+
   })
 })
