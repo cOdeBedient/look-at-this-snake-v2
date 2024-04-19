ed69cb169361cc859c157708a3447d5c69f3753e
Adam Bedient <adambedient@gmail.com>
2024-04-19 15:57:16 -0400

    Add styled error

---
+++ src/Error/Error.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledError = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    canvas {
+        background-color: rgb(175, 205, 165);
+    }
+`
