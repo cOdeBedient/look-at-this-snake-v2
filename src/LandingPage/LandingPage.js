989cf856846ff116e41388c0881105166536ff86
Adam Bedient <adambedient@gmail.com>
2024-04-19 16:08:16 -0400

    Add styled landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,13 +1,13 @@
-import './LandingPage.css'
+import { StyledLandingPage } from './LandingPage.styled'
 import LevelForm from '../LevelForm/LevelForm'
 
 export default function LandingPage({ filterSnakes }) {
 
     return (
-        <div className="landing">
+        <StyledLandingPage>
             <h1>Look at this Snake then Look at this Puppy</h1>
             <h2>A phobic deprogramming tool designed by someone with no credentials</h2>
             <LevelForm filterSnakes={filterSnakes} />
-        </div>
+        </StyledLandingPage>
     )
 }
